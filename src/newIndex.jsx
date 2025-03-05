aa17dae8f11f69f3d702203ff32967194118ef01
ayathree <nobanitaayathree333@gmail.com>
2025-03-06 01:54:16 +0600

    first commit

---
+++ src/newIndex.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const newIndex = () => {
+    return (
+        <div>
+            <p className='bg-sky-400'>poojs</p>
+        </div>
+    );
+};
+
+export default newIndex;
